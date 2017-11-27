a645d7aace96be4970560e14f8f2c2e4f9914ecd
Jeffery1993 <jefferyyew@zju.edu.cn>
2017-11-27 21:17:45 +0800

    Enable the SpringBootApplication and CommandLineRunner

--- src/main/java/com/jeffery/template/Application.java
+++ src/main/java/com/jeffery/template/Application.java
@@ -1,35 +1,41 @@
 package com.jeffery.template;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.jeffery.template.common.config.AppConfig;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
 
-	// @Bean
-	// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	// return new CommandLineRunner() {
-	//
-	// @SuppressWarnings("unused")
-	// private ApplicationContext ctx;
-	//
-	// public CommandLineRunner setApplicationContext(ApplicationContext ctx) {
-	// this.ctx = ctx;
-	// return this;
-	// }
-	//
-	// @Override
-	// public void run(String... ctx) {
-	// SpringApplication.run(Application.class, ctx);
-	// }
-	//
-	// }.setApplicationContext(ctx);
-	// }
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
+		new AppConfig();
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return new CommandLineRunner() {
+
+			@SuppressWarnings("unused")
+			private ApplicationContext ctx;
+
+			public CommandLineRunner setApplicationContext(ApplicationContext ctx) {
+				this.ctx = ctx;
+				return this;
+			}
+
+			@Override
+			public void run(String... args) {
+
+			}
+
+		}.setApplicationContext(ctx);
 	}
 
 }
